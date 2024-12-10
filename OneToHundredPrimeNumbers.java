687f0ba52faec2828164060cbdb8fbeffa06950f
Vibhas-05 <22h51a05b2@cmrcet.ac.in>
2024-12-10 11:11:51 +0530

    commit 5

--- OneToHundredPrimeNumbers.java
+++ OneToHundredPrimeNumbers.java
@@ -1,6 +1,6 @@
 public class OneToHundredPrimeNumbers {
     public static void main(String[] args) {
-        System.out.println("PRIME NUMBERS BETWEEN 1 and 100 ARE:");
+        System.out.println("Prime Numbers Between 1 and 100 are:");
         for (int number = 2; number <=100; number++) {
             if (isPrime(number)) 
 {
